0a614a40c71a8e59e03dc5298845d07debf4d6f4
Fernanda <ffdearaujo90@gmail.com>
2023-01-10 13:40:12 -0300

    números organizados por ordem

--- loteria.js
+++ loteria.js
@@ -3,7 +3,10 @@ function numberSorter(n, max){
     for (i=1; i<=n; i++){
         numbers.push(Math.floor(Math.random()*max))
     }
-    console.log(numbers)
+    numbersAsc = numbers.sort(function(a, b) {
+        return a - b;
+    })
+    console.log(numbersAsc)
 }
 
 numberSorter(6,60)
